a290e3147f86b12f9d746068816e7f7c977d33c3
Bryn <bryne12336@gmail.com>
2022-09-29 11:11:45 +0100

    git hub issue fixed

---
+++ quiz.js
@@ -0,0 +1,7 @@
+// create function that shows panel after clicking "let's get started"
+
+//create function that hides carousel item- 1 on clicking it, and shows carousel item-2
+//create function that hides carousel item- 2 on clicking it, and shows carousel item-3
+//create function that hides carousel item- 4on clicking it, and shows carousel item-5
+//create function that hides carousel item- 5 on clicking it, and shows results
+// create function that counts how many correct answers are clickeed, and displays a message according to score.
